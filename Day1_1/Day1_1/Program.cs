37ad68749532bd3c1b0c62a333b54fd66559b9eb
rubens95 <rubens95@naver.com>
2018-10-27 12:23:48 +0900

    1일차 실습

---
+++ Day1_1/Day1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TypeCastExample1
+{
+    public static void Main()
+    {
+        //hort a = 10;
+        //t b = a;
+
+     int c = 50000;
+        try
+        {
+            short d = checked((short)c);
+            Console.WriteLine("Short : {0}", d);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ Day1_2/Day1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day1_2
+{
+    class Myconstants
+    {
+        public const double P1 = 3.14;    //멤버 필드
+        public const int MYAGE = 22;     // 멤버, 필드
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)    //프로그램 진입점
+        {
+           double raidus = 2;
+           double area = Myconstants.P1 * (raidus * raidus);
+           Console.WriteLine("Area = {0}, Age ={1}", area, Myconstants.MYAGE);
+
+           const string name = "홍길동";
+           Console.WriteLine("name : " + name);
+        }
+    }
+}
+
